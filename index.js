9b78105bf7f3baf9a9de908f8e52b29b63064272
Daniel Cheong <dcheo001@gmail.com>
2021-11-05 19:38:09 -0400

    commit tree 0396884b9d3d2662917d59bdbe7d21f23246b049

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const farmAnimals = 'cow horse sheep pig chicken';
+const farmAnimals = ['cow', 'horse', 'sheep', 'pig', 'chicken'];
 
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
 
@@ -46,3 +46,62 @@ const nestedMuppet = {
 // 7. Use destructuring to assign all variables using the keys as the variable names
 
 // 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner
+const sounds = {
+  moo: "cow",
+  neigh: "horse",
+  baa: "sheep",
+  oink: "pig",
+  cluck: "chicken"
+}
+const { moo, neigh, baa, oink, cluck } = sounds;
+const animals = {
+  bessie: "cow",
+  dolly: "sheep",
+  babe: "pig",
+  little: "chicken"
+}
+const { bessie, dolly, babe, little } = animals
+const diffColors = {
+  blackAndWhite: "cow",
+  black: "sheep",
+  pink: "pig"
+}
+const { blackAndWhite, black, pink } = diffColors
+const colorRainbow = {
+  red: "red",
+  orange: "orange",
+  yellow: "yellow",
+  green: "green",
+  blue: "blue",
+  indigo: "indigo",
+  violet: "violet"
+}
+const { red, orange, yellow, green, blue, indigo, violet } = colorRainbow
+const rainbow = {
+  r: "red",
+  o: "orange",
+  y: "yellow",
+  g: "green",
+  b: "blue",
+  v: "violet"
+}
+const { r, o, y, g, b, v } = rainbow
+const diffPurp = {
+  indg: "indigo"
+}
+const { indg } = diffPurp
+const objectDecon = {
+  muppetName: "Miss Piggy",
+  color: "pink",
+  song: "Never Before, Never Again",
+  job: "Cast member of The Muppet Show",
+  partner: "Kermit"
+}
+const { muppetName, color, song, job, partner } = objectDecon
+const songsToSing = {
+  song2: "Moving Right Along",
+  song4: "I Hope That Something Better Comes Along",
+  nestedJob: "Host of The Muppet Show",
+  nestedPartner: "Miss Piggy"
+}
+const { song2, song4, nestedJob, nestedPartner } = songsToSing
